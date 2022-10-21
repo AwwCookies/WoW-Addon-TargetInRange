03f5e654604a8c11599e660fc32073e9d2611265
AwwCookies <emma@emaw.dev>
2022-10-21 06:03:27 -0400

    Hello, World

---
+++ TargetInRange.lua
@@ -0,0 +1,32 @@
+local TargetInRangeFrame = CreateFrame("frame", "TargetInRangeFrame")
+local TIRString = TargetInRangeFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+
+TargetInRangeFrame:RegisterEvent("PLAYER_TARGET_CHANGED")
+
+TargetInRangeFrame:SetScript("OnEvent", function(self, event, ...)
+      if(event == "PLAYER_TARGET_CHANGED") then
+         TIRString:SetText(tostring(IsActionInRange(1)))
+      end
+end)
+
+TargetInRangeFrame:SetScript("OnUpdate", function()
+    if (UnitExists("target")) then
+        local nameplate = C_NamePlate.GetNamePlateForUnit("target")
+        if (nameplate == nil) then
+            TIRString:Hide()
+            return
+        end
+        TIRString:SetPoint("CENTER", nameplate, "TOP", 0, 0)
+        if (IsActionInRange(1)) then
+            TIRString:SetTextColor(0, 255, 0, 1)
+            TIRString:SetText("In Range")
+        else
+            TIRString:SetTextColor(255, 0, 0, 0.5)
+            TIRString:SetText("Out of Range")
+        end
+        TIRString:Show()
+    else
+        TIRString:SetText("No Target")
+        TIRString:Hide()
+    end
+end)
